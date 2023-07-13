d4c07135b1bd7afb357688cc0299b5b2165d1a3d
Denis AKPAGNONITE <akpagnonited@gmail.com>
2023-07-13 12:50:09 +0100

    added components

---
+++ resources/js/Components/Alert.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { Alert } from "flowbite-vue";
+
+defineProps({
+    type: {
+        type: String,
+        default: 'success',
+        validator(value) {
+            return ['info', 'danger', 'success', 'warning', 'dark'].includes(value);
+        },
+    },
+    message: {
+        type: String,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <div class="flex flex-col">
+        <Alert :type="type" closable>{{ message }}</Alert>
+    </div>
+</template>
