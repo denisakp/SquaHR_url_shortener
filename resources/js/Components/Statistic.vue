6d6446475e65329fc9c0fd89c83785555d2d659d
Denis AKPAGNONITE <akpagnonited@gmail.com>
2023-07-13 13:23:49 +0100

    ...

--- resources/js/Components/Statistic.vue
+++ resources/js/Components/Statistic.vue
@@ -3,7 +3,6 @@ defineProps(["unique_visitors", "total", "most_clicked"]);
 </script>
 
 <template>
-    <!-- component -->
     <div class="grid gap-6 mb-6 md:grid-cols-2 xl:grid-cols-3">
         <div class="min-w-0 rounded-lg shadow-xs overflow-hidden">
             <div class="p-4 flex items-center border border-black">
